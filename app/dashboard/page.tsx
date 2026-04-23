edfb5ba2427bd1a0a888ca20f70dd17b4a682d47
app-dev-happy <appdev.happy@gmail.com>
2026-04-23 12:27:55 +0530

    + added Banner Ads caraosual widget + added greetings widget

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,11 @@
-import ProfileCompletionCard from '@/components/ProfileCompletionCard';
+import BannerAdsCarousel from '@/components/dashboard/widgets/BannerAdsCarousel';
+import GreetingWidget from '@/components/dashboard/widgets/GreetingWidget';
 
 export default async function DashboardPage() {
-  
+  return (
+    <div>
+      <BannerAdsCarousel showIndicators={false} />
+      <GreetingWidget username="Shubham" />
+    </div>
+  );
 }
